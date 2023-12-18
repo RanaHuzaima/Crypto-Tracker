e81a55c50e0571621e28ca81a0630fd6af9e359e
Rana Huzaima <huzaimadigital@gmail.com>
2023-12-18 19:14:27 +0500

    Make  CoinList Component

--- src/Components/CoinList.jsx
+++ src/Components/CoinList.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CoinList = () => {
-  return (
-    <div>
-      Coinlist
-    </div>
-  )
-}
+  const [data, setdata] = useState([]);
 
-export default CoinList
+  return (
+    <section>
+      <div classNameName="text-2xl font-bold mb-3 mt-3">Market</div>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Coin
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default CoinList;

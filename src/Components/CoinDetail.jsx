3e5f9641d7a6a6d95c74f88e99fb27b7ecfe1364
Rana Huzaima <huzaimadigital@gmail.com>
2023-12-18 23:18:03 +0500

    Make CoinDetails Component

--- src/Components/CoinDetail.jsx
+++ src/Components/CoinDetail.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CoinDetail = () => {
-  return <div>details</div>;
+  const { id } = useParams();
+  const [data, setData] = useState({});
+  const fetchdata = async () => {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&sparkline=false`
+    );
+    const data = await res.json();
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  return (
+    <>
+      {Object.keys(data).length > 0 ? (
+        <div>
+          <div className=" flex items-center gap-2 ">
+            <img
+              src={data.image.small}
+              alt={data.id}
+              className=" rounded-full w-16"
+            />
+            <span className=" text-2xl font-bold">{data.name}</span>
+          </div>
+          <p
+            className=" [&>a]:text-blue-400 [&>a]:underline"
+            dangerouslySetInnerHTML={{ __html: data.description.en }}
+          ></p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+  //   return !data > 0 ? (
+  //     <div>
+  //       <div className=" flex items-center gap-2 ">
+  //         <img
+  //           src={data.image.small}
+  //           alt={data.id}
+  //           className=" rounded-full w-16"
+  //         />
+  //         <span className=" text-2xl font-bold">{data.name}</span>
+  //       </div>
+  //       <p
+  //         className=" [&>a]:text-blue-400 [&>a]:underline"
+  //         dangerouslySetInnerHTML={{ __html: data.description.en }}
+  //       ></p>
+  //     </div>
+  //   ) : (
+  //     <div>Loading</div>
+  //   );
 };
 
 export default CoinDetail;
--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HistoryChart from "../Components/HistoryChart";
+import CoinDetail from "../Components/CoinDetail";
 
 const CoinPage = () => {
   return (
     <section>
       <div className="max-w-screen-xl mx-auto p-4">
         <HistoryChart />
+        <CoinDetail />
       </div>
     </section>
   );

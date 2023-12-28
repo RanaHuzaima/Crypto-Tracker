ef4500bee777a7b874edbc17615601a35881d51f
Rana Huzaima <huzaimadigital@gmail.com>
2023-12-28 23:18:06 +0500

    Fix Some Css Bug

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,27 +6,20 @@ const HomePage = () => {
     <>
       <section className="max-w-screen-xl mx-auto">
         <div className="flex flex-col bg-white">
-          <div className="grid w-full grid-cols-1 my-auto md:grid-cols-2 xl:gap-14 md:gap-5">
-            <div className=" flex flex-col justify-center col-span-1 text-center lg:text-start">
-              <h1 className="mb-4 text-4xl font-extrabold leading-tight lg:text-8xl">
+          <div className="grid grid-rows-1 text-center md:text-start w-full my-auto md:grid-cols-2 xl:gap-14 md:gap-5">
+            <div className=" flex flex-col md:justify-center col-span-1 lg:text-start md:mt-0 px-4">
+              <h1 className="md:mb-4 text-4xl font-extrabold leading-tight md:text-8xl">
                 <span>CRYPTO TRACKER</span>
               </h1>
-              <h2 className="mb-6 text-4xl font-extrabold leading-tight lg:text-6xl">
+              <h2 className="mb-4 md:mb-6 text-4xl font-extrabold leading-tight md:text-6xl ">
                 REAL TIME.
               </h2>
 
-              <p className="mb-6 text-xl font-semibold leading-7 lg:w-3/4 text-black">
+              <p className="mb-4 md:mb-6 text-xl font-semibold leading-7 lg:w-3/4 text-black">
                 Track Crypto through a public api in real time. Visit the
                 Dashboard and more!
               </p>
-              <div className="flex flex-col items-center gap-4 lg:flex-row">
-                {/* <Button
-                  BtnText={"Dashborad"}
-                  px={5}
-                  py={2}
-                  bgColor={"black"}
-                  hoverbg={"white"}
-                /> */}
+              <div className="flex items-center justify-center md:justify-start gap-4 lg:flex-row">
                 <Button
                   BtnText={"Dashboard"}
                   px={5}
@@ -44,7 +37,7 @@ const HomePage = () => {
                 />
               </div>
             </div>
-            <div className="items-center justify-end hidden col-span-1 md:flex">
+            <div className="items-center md:justify-end col-span-1 flex order-first md:order-last ">
               <img
                 className="w-full rounded-md"
                 src="/HomePage.gif"
